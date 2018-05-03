2dd6eee3d9cf7cdb89441a801b08e026400c7fdf
woodbury shortridge <whshortridge@gmail.com>
2018-05-03 16:26:42 -0400

    face find test

--- findFace.py
+++ findFace.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import time
+import imutils
 
 # cascade for face detection
 face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
@@ -18,16 +19,19 @@ def lookforfaces():
         ret, frame = cap.read()
 
         # detect faces in video
+        frame = imutils.resize(frame, width=300)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # create median blur for diff test
-        median = cv2.medianBlur(gray,15)
+        # median = cv2.medianBlur(gray,15)
         # find faces
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         blank = ()
         num += 1
         if faces != blank:
+            print ('face')
             # test blur
+            '''
             blur = cv2.Laplacian(gray, cv2.CV_64F).var()
             if blur > blurthresh:
                 print ('blur face')
@@ -43,7 +47,9 @@ def lookforfaces():
                 if delta > changethresh:
                     print('good different face')
                     cv2.imwrite('img/face/face'+str(num)+'.jpg',frame)
-        time.sleep(2)
+            
+            '''
+        # time.sleep(2)
 
     # release capture
     cap.release()
